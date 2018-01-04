0688f953fc19060da7cf4a8f4d7c2b4ad18ae7a7
Patrick Dougherty <patrick.doc@ameritech.net>
2018-01-04 13:06:10 -0600

    Add query tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -14,6 +14,8 @@ type TestGraph = G.Gr Int Char
 
 main :: IO ()
 main = hspec $ describe "Strict Graphs" $ do
+
+    -- Construction
     describe "Construction" $ do
         describe "empty" $ do
             let e = G.empty :: TestGraph
@@ -55,6 +57,8 @@ main = hspec $ describe "Strict Graphs" $ do
             prop "makes the correct set of edges" $
                 \ns -> let es = G.edges (G.mkGraph (edgeList ns) ns :: TestGraph)
                        in HS.fromList es === HS.fromList (edgeList ns)
+
+    -- Basic Interface
     describe "Basic Interface" $ do
         describe "null" $ do
             it "is described throughout" $ True `shouldBe` True
@@ -74,9 +78,7 @@ main = hspec $ describe "Strict Graphs" $ do
                 G.matchAny gr `shouldBe` Just (G.Context' HS.empty 'a' HS.empty, G.empty)
             it "does not find a node when the graph is empty" $
                 G.matchAny (G.empty :: TestGraph) `shouldBe` Nothing
-{-
-        describe "(&)" $ do
--}
+        --describe "(&)" $ do
         describe "(!)" $ do
             let gr = G.mkGraph [] ['a'] :: TestGraph
             it "finds nodes in the graph" $
@@ -93,6 +95,8 @@ main = hspec $ describe "Strict Graphs" $ do
             it "is described throughout" $ True `shouldBe` True
         describe "edges" $ do
             it "is described throughout" $ True `shouldBe` True
+
+    -- Maps
     describe "Maps" $ do
         let fn = (:[])
             fe = (*(-1))
@@ -116,23 +120,95 @@ main = hspec $ describe "Strict Graphs" $ do
                                , G.Edge 'a' (-1) 'a' ]
         describe "node and Edge map" $ do
             it "is described by node map and edge map" $ True `shouldBe` True
-{-
+
+    -- Folds
     describe "Folds" $ do
         describe "foldr" $ do
+            prop "works like a list" $
+                \ns -> let graph = G.mkGraph [] ns :: TestGraph
+                       in length (G.foldr (:) [] graph)
+                          === length (foldr (:) [] (nub ns))
+
+    -- Queries
     describe "Queries" $ do
+        let edgeList ns = (\x y -> G.Edge x 1 y) <$> ns <*> ns
+            gr = G.mkGraph (tail (edgeList "abcde")) "abcde"
         describe "member" $ do
+            it "returns True for a node in the graph" $
+                G.member 'a' gr `shouldBe` True
+            it "returns False for a node not in the graph" $
+                G.member 'z' gr `shouldBe` False
         describe "neighbors" $ do
+            it "returns `Just lst` for a node in the graph" $
+                G.neighbors 'a' gr `shouldBe` Just "bcde"
+            it "returns `Nothing` for a node not in the graph" $
+                G.neighbors 'z' gr `shouldBe` Nothing
         describe "preds" $ do
+            it "returns `Just lst` for a node in the graph" $
+                G.preds 'a' gr `shouldBe` Just "bcde"
+            it "returns `Nothing` for a node not in the graph" $
+                G.preds 'z' gr `shouldBe` Nothing
         describe "succs" $ do
+            it "returns `Just lst` for a node in the graph" $
+                G.succs 'a' gr `shouldBe` Just "bcde"
+            it "returns `Nothing` for a node not in the graph" $
+                G.succs 'z' gr `shouldBe` Nothing
         describe "inEdges" $ do
+            it "returns `Just lst` for a node in the graph" $
+                G.inEdges 'a' gr `shouldBe` (Just [ G.Edge 'b' 1 'a'
+                                                  , G.Edge 'c' 1 'a'
+                                                  , G.Edge 'd' 1 'a'
+                                                  , G.Edge 'e' 1 'a' ] :: Maybe [G.Edge Int Char])
+            it "returns `Nothing` for a node not in the graph" $
+                G.inEdges 'z' gr `shouldBe` Nothing
         describe "outEdges" $ do
+            it "returns `Just lst` for a node in the graph" $
+                G.outEdges 'a' gr `shouldBe` (Just [ G.Edge 'a' 1 'b'
+                                                   , G.Edge 'a' 1 'c'
+                                                   , G.Edge 'a' 1 'd'
+                                                   , G.Edge 'a' 1 'e' ] :: Maybe [G.Edge Int Char])
+            it "returns `Nothing` for a node not in the graph" $
+                G.outEdges 'z' gr `shouldBe` Nothing
         describe "inDegree" $ do
+            it "returns `Just int` for a node in the graph" $
+                G.inDegree 'b' gr `shouldBe` Just 5
+            it "returns `Nothing` for a node not in the graph" $
+                G.inDegree 'z' gr `shouldBe` Nothing
         describe "outDegree" $ do
+            it "returns `Just int` for a node in the graph" $
+                G.outDegree 'b' gr `shouldBe` Just 5
+            it "returns `Nothing` for a node not in the graph" $
+                G.outDegree 'z' gr `shouldBe` Nothing
         describe "degree" $ do
+            it "returns `Just int` for a node in the graph" $
+                G.degree 'b' gr `shouldBe` Just 10
+            it "returns `Nothing` for a node not in the graph" $
+                G.degree 'z' gr `shouldBe` Nothing
         describe "hasEdge" $ do
+            it "returns `True` for an edge in the graph" $
+                G.hasEdge (G.Edge 'a' 1 'b' :: G.Edge Int Char) gr `shouldBe` True
+            it "returns `False` for an edge not in the graph" $
+                G.hasEdge (G.Edge 'a' 1 'a' :: G.Edge Int Char) gr `shouldBe` False
         describe "hasNeighbor" $ do
+            it "returns `True` if the second node is a neighbor of the first" $
+                G.hasNeighbor 'a' 'b' gr `shouldBe` True
+            it "returns `False` otherwise" $
+                G.hasNeighbor 'z' 'a' gr `shouldBe` False
+{-
+    -- Filters
     describe "Filters" $ do
         describe "node filter" $ do
         describe "edge filter" $ do
+    -- Insertion and Deletion
     describe "Insertion and Deletion" $ do
+        describe "insNode" $ do
+        describe "safeInsNode" $ do
+        describe "delNode" $ do
+        describe "insEdge" $ do
+        describe "delHeads" $ do
+        describe "delTails" $ do
+        describe "insHead" $ do
+        describe "delHead" $ do
+        describe "insTail" $ do
+        describe "delTail" $ do
 -}
